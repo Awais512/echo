7144a75fd80d921bccb3b687303119615732a1a0
awais512 <raza.awais25@gmail.com>
2025-08-30 13:49:31 +0100

    Added complete conversation panel and skeleton for loading

--- apps/web/modules/dashboard/ui/components/conversations-panel.tsx
+++ apps/web/modules/dashboard/ui/components/conversations-panel.tsx
@@ -2,21 +2,22 @@
 
 import React from "react";
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@workspace/ui/components/select";
-import {
   ListIcon,
   ArrowRightIcon,
   ArrowUpIcon,
   CheckIcon,
   CornerUpLeftIcon,
 } from "lucide-react";
-
+import { useAtomValue, useSetAtom } from "jotai/react";
 import { formatDistanceToNow } from "date-fns";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@workspace/ui/components/select";
 
 import { ScrollArea } from "@workspace/ui/components/scroll-area";
 import { usePaginatedQuery } from "convex/react";
@@ -28,7 +29,9 @@ import { usePathname } from "next/navigation";
 import { DicebearAvatar } from "@workspace/ui/components/dicebear-avatar";
 import { ConversationStatusIcon } from "@workspace/ui/components/conversation-status-icon";
 import { statusFilterAtom } from "../../atoms";
-import { useAtomValue, useSetAtom } from "jotai/react";
+import { InfiniteScrollTrigger } from "@workspace/ui/components/infinite-scroll-trigger";
+import { useInfiniteScroll } from "@workspace/ui/hooks/use-infinite-scroll";
+import { Skeleton } from "@workspace/ui/components/skeleton";
 
 export const ConversationsPanel = () => {
   const pathname = usePathname();
@@ -44,6 +47,18 @@ export const ConversationsPanel = () => {
       initialNumItems: 10,
     }
   );
+
+  const {
+    topElementRef,
+    handleLoadMore,
+    canLoadMore,
+    isLoadingMore,
+    isLoadingFirstPage,
+  } = useInfiniteScroll({
+    status: conversations.status,
+    loadMore: conversations.loadMore,
+    loadSize: 10,
+  });
 
   return (
     <div className="flex flex-col h-full w-full bg-background text-sidebar-foreground">
@@ -88,73 +103,109 @@ export const ConversationsPanel = () => {
           </SelectContent>
         </Select>
       </div>
-      <ScrollArea className="max-h-[calc(100vh-53px)]">
-        <div className="flex w-full flex-1 flex-col text-sm">
-          {conversations.results.map((conversation) => {
-            const isLastMessageFromOperator =
-              conversation.lastMessage?.message?.role !== "user";
 
-            const country = getCountryFromTimezone(
-              conversation.contactSession.metadata?.timezone
-            );
-            const countryFlagUrl = country?.code
-              ? getCountryFlagUrl(country.code)
-              : undefined;
+      {isLoadingFirstPage ? (
+        <SkeletonConversations />
+      ) : (
+        <ScrollArea className="max-h-[calc(100vh-53px)]">
+          <div className="flex w-full flex-1 flex-col text-sm">
+            {conversations.results.map((conversation) => {
+              const isLastMessageFromOperator =
+                conversation.lastMessage?.message?.role !== "user";
 
-            return (
-              <Link
-                key={conversation._id}
-                href={`/conversations/${conversation._id}`}
-                className={cn(
-                  "relative flex cursor-pointer items-start gap-3 border-b p-4 py-5 text-sm leading-tight hover:bg-accent hover:text-accent-foreground",
-                  pathname === `/conversations/${conversation._id}` &&
-                    "bg-accent text-accent-foreground"
-                )}
-              >
-                <div
+              const country = getCountryFromTimezone(
+                conversation.contactSession.metadata?.timezone
+              );
+              const countryFlagUrl = country?.code
+                ? getCountryFlagUrl(country.code)
+                : undefined;
+
+              return (
+                <Link
+                  key={conversation._id}
+                  href={`/conversations/${conversation._id}`}
                   className={cn(
-                    "-translate-y-1/2 absolute top-1/2 left-0 h-[64%] w-1 rounded-r-full bg-neutral-300 opacity-0 transition-opacity",
+                    "relative flex cursor-pointer items-start gap-3 border-b p-4 py-5 text-sm leading-tight hover:bg-accent hover:text-accent-foreground",
                     pathname === `/conversations/${conversation._id}` &&
-                      "opacity-100"
+                      "bg-accent text-accent-foreground"
                   )}
-                />
-                <DicebearAvatar
-                  seed={conversation.contactSession._id}
-                  badgeImageUrl={countryFlagUrl}
-                  size={40}
-                  className="shrink-0"
-                />
-                <div className="flex-1">
-                  <div className="flex w-full items-center gap-2">
-                    <span className="truncate font-bold">
-                      {conversation.contactSession.name}
-                    </span>
-                    <span className="ml-auto shrink-0 text-muted-foreground text-xs">
-                      {formatDistanceToNow(conversation._creationTime)}
-                    </span>
-                  </div>
-                  <div className="mt-1 flex items-center justify-between gap-2">
-                    <div className="flex w-0 grow items-center gap-1">
-                      {isLastMessageFromOperator && (
-                        <CornerUpLeftIcon className="size-3 shrink-0 text-muted-foreground" />
-                      )}
-                      <span
-                        className={cn(
-                          "line-clamp-1 text-muted-foreground text-xs",
-                          !isLastMessageFromOperator && "font-bold text-black"
-                        )}
-                      >
-                        {conversation.lastMessage?.text}
+                >
+                  <div
+                    className={cn(
+                      "-translate-y-1/2 absolute top-1/2 left-0 h-[64%] w-1 rounded-r-full bg-neutral-300 opacity-0 transition-opacity",
+                      pathname === `/conversations/${conversation._id}` &&
+                        "opacity-100"
+                    )}
+                  />
+                  <DicebearAvatar
+                    seed={conversation.contactSession._id}
+                    badgeImageUrl={countryFlagUrl}
+                    size={40}
+                    className="shrink-0"
+                  />
+                  <div className="flex-1">
+                    <div className="flex w-full items-center gap-2">
+                      <span className="truncate font-bold">
+                        {conversation.contactSession.name}
+                      </span>
+                      <span className="ml-auto shrink-0 text-muted-foreground text-xs">
+                        {formatDistanceToNow(conversation._creationTime)}
                       </span>
                     </div>
-                    <ConversationStatusIcon status={conversation.status} />
+                    <div className="mt-1 flex items-center justify-between gap-2">
+                      <div className="flex w-0 grow items-center gap-1">
+                        {isLastMessageFromOperator && (
+                          <CornerUpLeftIcon className="size-3 shrink-0 text-muted-foreground" />
+                        )}
+                        <span
+                          className={cn(
+                            "line-clamp-1 text-muted-foreground text-xs",
+                            !isLastMessageFromOperator && "font-bold text-black"
+                          )}
+                        >
+                          {conversation.lastMessage?.text}
+                        </span>
+                      </div>
+                      <ConversationStatusIcon status={conversation.status} />
+                    </div>
                   </div>
+                </Link>
+              );
+            })}
+            <InfiniteScrollTrigger
+              canLoadMore={canLoadMore}
+              isLoadingMore={isLoadingMore}
+              onLoadMore={handleLoadMore}
+              ref={topElementRef}
+            />
+          </div>
+        </ScrollArea>
+      )}
+    </div>
+  );
+};
+
+export const SkeletonConversations = () => {
+  return (
+    <div className="flex min-h-0 flex-1 flex-col gap-2 overflow-auto">
+      <div className="relative flex w-full min-w-0 flex-col p-2">
+        <div className="w-full space-y-2">
+          {Array.from({ length: 8 }).map((_, index) => (
+            <div key={index} className="flex items-start gap-3 rounded-lg p-4">
+              <Skeleton className="h-10 w-10 shrink-0 rounded-full" />
+              <div className="min-w-0 flex-1">
+                <div className="flecx w-full items-center gap-2">
+                  <Skeleton className="h-4 w-24" />
+                  <Skeleton className="ml-auto h-3 w-12 shrink-0" />
                 </div>
-              </Link>
-            );
-          })}
+                <div className="mt-2">
+                  <Skeleton className="h-3 w-full" />
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
-      </ScrollArea>
+      </div>
     </div>
   );
 };
