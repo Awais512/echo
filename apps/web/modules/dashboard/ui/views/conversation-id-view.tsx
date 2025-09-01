6edfaf3ffff0bc00dbcb66880f10690554e4ce6a
awais512 <raza.awais25@gmail.com>
2025-09-01 12:58:46 +0100

    Added loading skeleton to the chat

--- apps/web/modules/dashboard/ui/views/conversation-id-view.tsx
+++ apps/web/modules/dashboard/ui/views/conversation-id-view.tsx
@@ -32,6 +32,8 @@ import { toUIMessages, useThreadMessages } from "@convex-dev/agent/react";
 import { DicebearAvatar } from "@workspace/ui/components/dicebear-avatar";
 import { ConversationStatusButton } from "../components/conversation-status-button";
 import { useState } from "react";
+import { cn } from "@workspace/ui/lib/utils";
+import { Skeleton } from "@workspace/ui/components/skeleton";
 
 interface ConversationIdViewProps {
   conversationId: Id<"conversations">;
@@ -138,6 +140,10 @@ export const ConversationIdView = ({
     }
   };
 
+  if (conversation === undefined || messages.status === "LoadingFirstPage") {
+    return <ConversationIdViewLoading />;
+  }
+
   return (
     <div className="flex h-full flex-col bg-muted">
       <header className="flex items-center justify-between border-b bg-background p-2.5">
@@ -237,6 +243,55 @@ export const ConversationIdView = ({
             </AIInputToolbar>
           </AIInput>
         </Form>
+      </div>
+    </div>
+  );
+};
+
+export const ConversationIdViewLoading = () => {
+  return (
+    <div className="flex h-full flex-col bg-muted">
+      <header className="flex items-center justify-between border-b bg-background p-2.5">
+        <Button disabled size="sm" variant="ghost">
+          <MoreHorizontalIcon />
+        </Button>
+      </header>
+      <AIConversation className="max-h-[calc(100vh-180px)]">
+        <AIConversationContent>
+          {Array.from({ length: 8 }, (_, index) => {
+            const isUser = index % 2 === 0;
+            const widths = ["w-48", "w-60", "w-72"];
+            const width = widths[index % widths.length];
+
+            return (
+              <div
+                className={cn(
+                  "group flex w-full items-end justify-end gap-2 py-2 [&>div]:max-w-[80%]",
+                  isUser ? "is-user" : "is-assistant flex-row-reverse"
+                )}
+                key={index}
+              >
+                <Skeleton
+                  className={`h-9 ${width} rounded-lg bg-neutral-200`}
+                />
+                <Skeleton className="size-8 rounded-full bg-neutral-200" />
+              </div>
+            );
+          })}
+        </AIConversationContent>
+      </AIConversation>
+
+      <div className="p-2">
+        <AIInput>
+          <AIInputTextarea
+            disabled
+            placeholder="Type your response as an operator"
+          />
+          <AIInputToolbar>
+            <AIInputTools />
+            <AIInputSubmit disabled status="ready" />
+          </AIInputToolbar>
+        </AIInput>
       </div>
     </div>
   );
