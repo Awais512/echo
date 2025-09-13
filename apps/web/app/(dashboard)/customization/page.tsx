5dedb1c98ac88ec3791e0252fbf1c4d9da668f3f
awais512 <raza.awais25@gmail.com>
2025-09-14 00:13:14 +0100

    Widget customization functionality done

--- apps/web/app/(dashboard)/customization/page.tsx
+++ apps/web/app/(dashboard)/customization/page.tsx
@@ -1,7 +1,8 @@
+import { CustomizationView } from "@/modules/customization/ui/views/customization-view";
 import React from "react";
 
 const Page = () => {
-  return <div>Customization Page</div>;
+  return <CustomizationView />;
 };
 
 export default Page;
---
+++ apps/web/modules/customization/ui/components/vapi-form-fields.tsx
@@ -0,0 +1,121 @@
+import React from "react";
+import { UseFormReturn } from "react-hook-form";
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@workspace/ui/components/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@workspace/ui/components/select";
+
+import {
+  useVapiAssistants,
+  useVapiPhoneNumbers,
+} from "@/modules/plugins/hooks/use-vapi-data";
+import { FormSchema } from "../../types";
+
+interface VapiFormFieldsProps {
+  form: UseFormReturn<FormSchema>;
+}
+
+export const VapiFormFields = ({ form }: VapiFormFieldsProps) => {
+  const { data: assistants, isLoading: assistantLoading } = useVapiAssistants();
+  const { data: phoneNumbers, isLoading: phoneNumbersLoading } =
+    useVapiPhoneNumbers();
+
+  const disabled = form.formState.isSubmitting;
+
+  return (
+    <>
+      <FormField
+        control={form.control}
+        name="vapiSettings.assistanceId"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Voice AssistANT</FormLabel>
+
+            <Select
+              value={field.value}
+              onValueChange={field.onChange}
+              disabled={assistantLoading || disabled}
+            >
+              <FormControl>
+                <SelectTrigger>
+                  <SelectValue
+                    placeholder={
+                      assistantLoading
+                        ? "Loading assistants"
+                        : "Select an assistant"
+                    }
+                  />
+                </SelectTrigger>
+              </FormControl>
+              <SelectContent>
+                <SelectItem value="none">None</SelectItem>
+                {assistants.map((assistant) => (
+                  <SelectItem value={assistant.id} key={assistant.id}>
+                    {assistant.name || "Unnamed Assistant"} -{" "}
+                    {assistant.model?.model || "Unknown model"}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+            <FormDescription>
+              The Vapi Assistant to use for voice calls
+            </FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+
+      <FormField
+        control={form.control}
+        name="vapiSettings.phoneNumber"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Display Phone Assistant</FormLabel>
+
+            <Select
+              value={field.value}
+              onValueChange={field.onChange}
+              disabled={phoneNumbersLoading || disabled}
+            >
+              <FormControl>
+                <SelectTrigger>
+                  <SelectValue
+                    placeholder={
+                      assistantLoading
+                        ? "Loading phone numbers"
+                        : "Select a phone numnber"
+                    }
+                  />
+                </SelectTrigger>
+              </FormControl>
+              <SelectContent>
+                <SelectItem value="none">None</SelectItem>
+                {phoneNumbers.map((phoneNumber) => (
+                  <SelectItem value={phoneNumber.id} key={phoneNumber.id}>
+                    {phoneNumber.name || "Unknown"} -{" "}
+                    {phoneNumber.name || "Unnamed"}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+            <FormDescription>
+              Phone numbers to display in the widget
+            </FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+    </>
+  );
+};
