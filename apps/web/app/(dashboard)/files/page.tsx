6decfb57254b6c28f01c1e166d52a757ecd6b822
awais512 <raza.awais25@gmail.com>
2025-09-27 22:05:42 +0100

    Added subscription schema, UI Protection, Billing Page

--- apps/web/app/(dashboard)/files/page.tsx
+++ apps/web/app/(dashboard)/files/page.tsx
@@ -1,8 +1,20 @@
+import { Protect } from "@clerk/nextjs";
 import { FilesView } from "@/modules/files/ui/views/files-view";
-import React from "react";
+import { PremiiumFeatureOverlay } from "@/modules/billing/ui/components/premium-feature-overlay";
 
 const Page = () => {
-  return <FilesView />;
+  return (
+    <Protect
+      condition={(has) => has({ plan: "pro" })}
+      fallback={
+        <PremiiumFeatureOverlay>
+          <FilesView />
+        </PremiiumFeatureOverlay>
+      }
+    >
+      <FilesView />
+    </Protect>
+  );
 };
 
 export default Page;
