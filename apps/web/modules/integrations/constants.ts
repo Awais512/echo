7f6a22d876784f002e828e1d43a1b365ee607f1d
awais512 <raza.awais25@gmail.com>
2025-09-30 23:50:27 +0100

    Added integration dialog

---
+++ apps/web/modules/integrations/constants.ts
@@ -0,0 +1,22 @@
+export const INTEGRATIONS = [
+  {
+    id: "html",
+    title: "HTML",
+    icon: "/languages/html5.svg",
+  },
+  {
+    id: "react",
+    title: "React",
+    icon: "/languages/react.svg",
+  },
+  {
+    id: "nextjs",
+    title: "Next.js",
+    icon: "/languages/nextjs.svg",
+  },
+  {
+    id: "javascript",
+    title: "JavaScript",
+    icon: "/languages/javascript.svg",
+  },
+];
